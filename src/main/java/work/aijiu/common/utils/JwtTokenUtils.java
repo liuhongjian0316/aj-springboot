39818f5cfd1c9c79cc0f3f619b10b33438481983
liuhongjian <liuhongjian0316@outlook.comgit config --global user.email liuhongjian0316@outlook.com>
2020-11-27 14:37:20 +0800

    banner

--- src/main/java/work/aijiu/common/utils/JwtTokenUtils.java
+++ src/main/java/work/aijiu/common/utils/JwtTokenUtils.java
@@ -16,8 +16,8 @@ public class JwtTokenUtils {
     public static final String TOKEN_HEADER = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer ";
 
-    private static final String SECRET = "aj";
-    private static final String ISS = "aj";
+    private static final String SECRET = "jwtsecretdemo";
+    private static final String ISS = "echisan";
 
     // 角色的key
     private static final String ROLE_CLAIMS = "rol";
